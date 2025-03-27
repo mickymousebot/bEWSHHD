65c1c8a2c8486357381e1821c9563ddfbd498a68
Micky Mouse <techsashif+eufya@gmail.com>
2025-03-28 00:07:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from bot import Bot
 import pyrogram.utils
 
-pyrogram.utils.MIN_CHANNEL_ID = -1002354118780 #Add Db Channel id Here -10000000000
+pyrogram.utils.MIN_CHANNEL_ID = -1002669922545 #Add Db Channel id Here -10000000000
 Bot().run()
